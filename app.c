5b4db6ca89d04ed36b2acd6150d613048b5b7534
Denis <dhoornae@ulb.ac.be>
2018-04-16 23:35:14 +0200

    Addition of slight modifications (mainly graphical) that are yet to be verified

--- app.c
+++ app.c
@@ -45,8 +45,8 @@
 *********************************************************************************************************
 */
 #define	 APP_TASK_1_PERIOD						10
-#define	 Timer_Task_PERIOD						100
-#define	 Keyboard_Task_PERIOD					50
+#define	 Timer_Task_PERIOD						100 // useless ??
+#define	 Keyboard_Task_PERIOD					50  // set to 100
 #define  Password_Management_Task_PERIOD		100
 #define  Button_handler_Task_PERIOD				100
 
@@ -111,9 +111,9 @@ CPU_INT16S  main (void) {
 	CPU_INT08U  err;
 
 	BSP_IntDisAll();	// Disable all interrupts until we are ready to accept them
-	
+
 	OSInit();			// Initialize "uC/OS-II, The Real-Time Kernel"
-	
+
 	init_elec_h_410();
 
 	myBox = OSMboxCreate((void*)0);
@@ -164,7 +164,7 @@ static  void  AppStartTask (void *p_arg) {
    (void)p_arg;	// to avoid a warning message
 
     BSP_Init();		// Initialize BSP (Board Support Package) functions
-	
+
 #if OS_TASK_STAT_EN > 0
     OSStatInit();	// Determine CPU capacity
 #endif
@@ -183,7 +183,7 @@ static  void  AppStartTask (void *p_arg) {
 					OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 	// defines the App Name (for debug purpose)
     OSTaskNameSet(Password_Management_Task_PRIO, (CPU_INT08U *)"Password Management Task", &err);
-	
+
 	OSTaskCreateExt(TimerTask,
 					(void *)0,
 					(OS_STK *)&TimerTaskStk[0],
@@ -221,19 +221,28 @@ static  void  AppStartTask (void *p_arg) {
 	// defines the App Name (for debug purpose)
 	OSTaskNameSet(Button_handler_Task_PRIO, (CPU_INT08U *)"Button handler Task", &err);
 
-	OSTaskCreateExt(AppLCDTask, (void *)0, (OS_STK *)&AppLCDTaskStk[0], APP_TASK_LCD_PRIO, APP_TASK_LCD_PRIO, (OS_STK *)&AppLCDTaskStk[APP_TASK_LCD_STK_SIZE-1], APP_TASK_LCD_STK_SIZE, (void *)0, OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
+	OSTaskCreateExt(AppLCDTask,
+					(void *)0,
+					(OS_STK *)&AppLCDTaskStk[0],
+					APP_TASK_LCD_PRIO,
+					APP_TASK_LCD_PRIO,
+					(OS_STK *)&AppLCDTaskStk[APP_TASK_LCD_STK_SIZE-1],
+					APP_TASK_LCD_STK_SIZE,
+					(void *)0,
+					OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 	// defines the App Name (for debug purpose)
     OSTaskNameSet(APP_TASK_LCD_PRIO, (CPU_INT08U *)"LCD Task", &err);
-	
+
 	while(1) {
 		OSTaskSuspend(APP_TASK_START_PRIO);
 		OSTimeDly(500);	// waits 500ms
-   	} 
+   	}
 }
 
 void LockedSystemActOnCorrectPassword() {
     // Switch the to the 'unlocked system' state
     flagSystemUnlocked = 1;
+	OSMboxPost(lcdBox, "Unlocked");
     // System unlocked
     LATAbits.LATA1 = 1;
     // Buzzer desactivated !
@@ -246,6 +255,7 @@ void LockedSystemActOnCorrectPassword() {
 void UnlockedSystemActOnCorrectPassword() {
     // Switch the to the 'locked system' state
     flagSystemUnlocked = 0;
+	OSMboxPost(lcdBox, "Locked");
     // System unlocked
     LATAbits.LATA1 = 0;
 }
@@ -255,16 +265,15 @@ unsigned char strEqual(char* word1, char* word2) {
 }
 
 static  void PasswordManagementTask (void *p_arg) {
-	(void)p_arg;			// to avoid a warning message	
+	(void)p_arg;			// to avoid a warning message
 
 	INT8U err;
-	char userProvidedCode[4];
-	char* tmpCharPointer = userProvidedCode;
+	char* userProvidedCode;
     while(1) {
 		TASK_ENABLE1 = 1;
-        tmpCharPointer = OSMboxPend(myBox, 50, &err); // blocking instruction
+        userProvidedCode = OSMboxPend(myBox, 50, &err); // blocking instruction
 		if(err == OS_ERR_NONE) {
-	        if(strEqual(tmpCharPointer, systemProvidedCode)) {
+	        if(strEqual(userProvidedCode, systemProvidedCode)) {
 	            if(!flagSystemUnlocked) {
 	                LockedSystemActOnCorrectPassword();
 	            }
@@ -272,15 +281,18 @@ static  void PasswordManagementTask (void *p_arg) {
 	                UnlockedSystemActOnCorrectPassword();
 	            }
 	        }
+			else {
+				OSMboxPost(lcdBox, "Wrong pwd");
+			}
 		}
 		TASK_ENABLE1 = 0;
-		OSTimeDly(Password_Management_Task_PERIOD);
+		OSTimeDly(Password_Management_Task_PERIOD); // useful ?
     }
 }
 
 static  void  TimerTask (void *p_arg) {
 	(void)p_arg;			// to avoid a warning message
-	
+
 	char unsigned counter = 60;
 	TASK_ENABLE2 = 1;
     LATAbits.LATA2 = 1;
@@ -308,19 +320,51 @@ static void ButtonHandlerTask(void *p_arg) {
 		TASK_ENABLE3 = 0;
 		OSTimeDly(Button_handler_Task_PERIOD);
 	}
-} 
+}
 
-static  void  KeyboardTask (void *p_arg) {
-	(void)p_arg;			// to avoid a warning message
-	
-	KeyboardInit();
-	
+INT8U getch() {
 	INT8U key1 = 0;
 	INT8U key2 = 0;
 	INT8U key3;
-	char code[4];
-	char code2[4];
+
+	while(1) {
+		key3 = key2;
+		key2 = key1;
+		key1 = KeyboardScan();
+		if ((key1 == key2) && (key1 != key3) && (key1 != 255)) {
+			return hex2ASCII[key1];
+		}
+		OSTimeDly(Keyboard_Task_PERIOD); // TODO to reduce or useful ?
+	}
+}
+
+static  void  KeyboardTask (void *p_arg) {
+	(void)p_arg;			// to avoid a warning message
+	/*
+	KeyboardInit();
+
+	char code[2][4] = {"****", "****"};
     unsigned char i;
+    while(1) {
+		TASK_ENABLE4 = 1;
+        for(i=0; i<4; i++) {
+			code[0][i] = getch();
+			code[1][i] = code[0][i];
+			OSMboxPost(lcdBox, code[1]);
+        }
+        OSMboxPost(myBox, code[0]);
+		OSMboxPost(lcdBox, code[1]);
+		TASK_ENABLE4 = 0;
+		OSTimeDly(Keyboard_Task_PERIOD);
+    }*/
+	KeyboardInit();
+
+	INT8U key1 = 0;
+	INT8U key2 = 0;
+	INT8U key3;
+	char code[2][4] = {"****", "****"};
+    unsigned char i;
+
     while(1) {
 		TASK_ENABLE4 = 1;
 		i = 0;
@@ -329,36 +373,36 @@ static  void  KeyboardTask (void *p_arg) {
 			key2 = key1;
 			key1 = KeyboardScan();
 			if ((key1 == key2) && (key1 != key3) && (key1 != 255)) {
-				code[i] = hex2ASCII[key1];
-				code2[i] = hex2ASCII[key1];
+				code[0][i] = hex2ASCII[key1];
+				code[1][i] = hex2ASCII[key1];
+				OSMboxPost(lcdBox, code[1]);
 				i++;
 			}
-			OSTimeDly(Keyboard_Task_PERIOD);
+			// OSTimeDly(Keyboard_Task_PERIOD);
         }
-        OSMboxPost(myBox, code);
-		OSMboxPost(lcdBox, code2);
+        OSMboxPost(myBox, code[0]);
+		OSMboxPost(lcdBox, code[1]);
 		TASK_ENABLE4 = 0;
 		OSTimeDly(Keyboard_Task_PERIOD);
-    }
+}
 }
 
 static  void  AppLCDTask (void *p_arg) {
 	INT8U err;
 	INT8U* key;
-	INT8U row, line;
 
    (void)p_arg;			// to avoid a warning message
 
 	DispInit(2, 16);	// Initialize uC/LCD for a 2 row by 16 column display
 	DispClrScr();		// Clear the screen
-	row = 0;			// initialise the cursor position
-	line = 0;
+	INT8U row = 0;			// initialise the cursor position
+	INT8U line = 0;
 
 	while(1) {
 		TASK_ENABLE5 = 1;
 		key = OSMboxPend(lcdBox, 0, &err);
+		DispClrScr();
 		DispStr(line, row, key);
 		TASK_ENABLE5 = 0;
-		// OSTimeDly(500);
 	}
 }
